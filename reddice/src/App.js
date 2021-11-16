a3bf4dd4773aca0110c7cd038c1bb579a9cd1da6
Bhesh Sejawal <bsejawal@gmail.com>
2021-11-16 17:23:44 -0600

    README.md file modified

---
+++ reddice/src/App.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import Greetings from './components/Greetings'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Greetings />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ reddice/src/components/Greetings.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Greetings extends Component {
+      render() {
+            return (
+                  <div>
+                        <h1>Hello From Greetings</h1>
+                  </div>
+            );
+      }
+}
+
+export default Greetings;
